f841c99d9836f18fba107cd5def26a502b125cec
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-08-17 15:36:29 +0200

    Create build4netlify.sh

---
+++ client/build4netlify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## For netlify
+cd client
+##
+
+rm -rf dist/
+mkdir dist; 
+sed -e "s|%%FUNCTION_API_URL%%|${FUNCTION_API_URL}|" -e "s|%%PUBLIC_URL%%|${PUBLIC_URL}|" index.html > dist/index.html;
+
+cp -R assets dist/
