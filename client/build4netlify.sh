e20c5f368a3ca5367efa0ae9e4740bb88d8ea4f4
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-08-17 16:00:05 +0200

    Update build4netlify.sh

--- client/build4netlify.sh
+++ client/build4netlify.sh
@@ -9,6 +9,5 @@ echo "DEPLOY_URL =  ${DEPLOY_URL}"
 rm -rf dist/
 mkdir dist; 
 sed -e "s|%%FUNCTION_API_URL%%|${FUNCTION_API_URL}|" -e "s|%%PUBLIC_URL%%|${DEPLOY_URL}|" index.html > dist/index.html;
-echo ${
 
 cp -R assets dist/
